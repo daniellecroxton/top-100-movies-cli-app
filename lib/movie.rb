57f65955159eb20ebd1917861155b576953b82eb
daniellecroxton <danikrist@gmail.com>
2017-08-14 13:15:51 -0600

    coding movie.rb

--- lib/movie.rb
+++ lib/movie.rb
@@ -5,19 +5,25 @@ class Movie
   @@all = []
 
   def initialize(movie_hash)
-
+    @name = movie_hash[:name]
+    @@all << self
   end
 
   def self.create_from_collection(movies_array)
-
+    movies_array.each do | movie |
+      Movie.new(movie)
+    end
   end
 
   def add_details(details_hash)
-
+    details_hash.each do | attr, value |
+      self.send("#{attr}=", value)
+    end
+    self
   end
 
   def self.all
     @@all
   end
-  
+
 end
