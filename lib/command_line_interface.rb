e4b10af392ccd2ecfa2f242ac83c5e05b7a1ca86
daniellecroxton <danikrist@gmail.com>
2017-08-14 13:34:34 -0600

    added method to CLI file, updated run method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -7,7 +7,9 @@ class CommandLineInterface
   BASE_PATH = "http://www.rottentomatoes.com/"
 
   def run
-
+    create_movies
+    add_movie_details
+    display_movies
   end
 
   def create_movies
@@ -19,7 +21,12 @@ class CommandLineInterface
   end
 
   def display_movies
-
+    puts "Welcome! Which of Rotten Tomatoes' Top 100 Movies would you like to see?
+    Please enter '1-25', '26-50', '51-75', or '76-100':"
   end
-  
+
+  def display_movie_details
+    puts "To learn more about a specific movie, please enter the movie's rank, to return to the previous menu, type 'menu':"
+  end
+
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_top_100(top_100_url)
+    top_100_page = Nokogiri::HTML(open(top_100_url))
+    movies = []
+          binding.pry
+    top_100_page.css("#top_movies_main .table").each do | movie |
+      movie_name = movie.css("a.unstyled.articleLink").text
+      movies << {name: movie_name}
 
+    end
+    movies
   end
 
   def self.scrape_movie(details_url)
