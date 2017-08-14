8768287b6bdbc35df454b8eddd62ee861f464dad
daniellecroxton <danikrist@gmail.com>
2017-08-14 12:07:58 -0600

    set-up run file and declared classes and attr_accessors

---
+++ lib/command_line_interface.rb
@@ -0,0 +1,8 @@
+require_relative "../lib/scraper.rb"
+require_relative "../lib/movie.rb"
+require 'nokogiri'
+
+
+class CommandLineInterface
+
+end
---
+++ lib/movie.rb
@@ -0,0 +1,5 @@
+class movie
+
+  attr_accessor :title, :tomatometer_score, :audience_score, :release_date, :rating, :genre, :director, :synopsis
+
+end
