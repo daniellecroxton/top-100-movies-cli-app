d3364a6fd823a6ef3bb35f4a00cb7b3e56267a93
daniellecroxton <danikrist@gmail.com>
2017-08-14 13:39:06 -0600

    added URL to scrape_top_100

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,14 +3,13 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_top_100(top_100_url)
-    top_100_page = Nokogiri::HTML(open(top_100_url))
+  def self.scrape_top_100
+    top_100_page = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
     movies = []
-          binding.pry
     top_100_page.css("#top_movies_main .table").each do | movie |
       movie_name = movie.css("a.unstyled.articleLink").text
       movies << {name: movie_name}
-
+      # binding.pry
     end
     movies
   end
